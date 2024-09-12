487c58c9be524408e298bded8b21c0e8721aaaae
Doug Haacke <dhaacke@gmail.com>
2024-09-11 22:19:01 -0600

    Processing Notams

---
+++ AirBoss/Views/NotamAnnotationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotamAnnotationView.swift
+//  AirBoss
+//
+//  Created by Doug Haacke on 9/11/24.
+//
+
+import SwiftUI
+
+struct NotamAnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotamAnnotationView()
+}
